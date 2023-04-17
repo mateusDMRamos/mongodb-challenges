3a0604e1997240bb5e4a33f0cd31ec851d5f0a95
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-04-17 16:43:21 -0300

    feat: desafio 10 - traz apenas lanches com percentual de proteínas entre [30, 40]

---
+++ challenges/desafio10.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+  { valoresNutricionais: { 
+    $elemMatch: {
+      tipo: "proteínas",
+      percentual: { $gte: 30, $lte: 40 },
+    },
+  },
+},
+  {
+    nome: 1,
+    _id: 0,
+  },
+);
