389869fa9816524039f1791a1bdab1756165822f
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-04-17 16:38:20 -0300

    feat: desafio 9 - apenas retorna lanches com menos de 500 calorias

---
+++ challenges/desafio9.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+  { valoresNutricionais: { 
+    $elemMatch: {
+      tipo: "calorias",
+      quantidade: { $lt: 500 },
+    },
+  },
+},
+  {
+    nome: 1,
+    _id: 0,
+  },
+);
